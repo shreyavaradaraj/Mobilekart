4c331746c250bb8b02b829770d419e63be6dd73a
shreyavaradaraj <shreyalibra22@gmail.com>
2016-09-15 09:06:50 +0530

    Add files via upload

---
+++ estore/about.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<head>
+<title>About Us</title>
+</head>
+<body>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+<div class="container-fluid">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span> 
+<span class="icon-bar"></span> 
+</button>
+<a class="navbar-brand" href="#">E-Store</a>
+</div>
+<div class="collapse navbar-collapse" id="myNavbar">
+<ul class="nav navbar-nav navbar-right">
+  <li><a href="signup.php"><p><span class="glyphicon glyphicon-user"></span> Sign Up</p></a></li>
+  <li><a href="#"><p><span class="glyphicon glyphicon-log-out"></span> Login</p></a></li>
+  <li><a href="about.php"><p><span class="glyphicon glyphicon-tasks"></span> About us</p></a></li>
+  <li><a href="contact.php"><p><span class="glyphicon glyphicon-phone"></span> Contact Us</p></a></li>
+</div> 
+ </nav>
+ <br><br><br><br>
+ </body>
+ </html>
+ 
---
+++ estore/modal.js
@@ -0,0 +1,22 @@
+<button type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal">Login</button>
+  <div id="myModal" class="modal fade" role="dialog">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">X</button>
+<h4 class="modal-title">LOGIN</h4>
+</div>
+<div class="modal-body">
+<p>Don't have an Account? <span style="color:blue">Register</span></p>
+<div class="form-group">
+<input type="text" class="form-control" placeholder="Email"><br>
+	<input type="Password" class="form-control" placeholder="Password"><br>
+</form>
+<p><span style="color:blue">Forgot Password?</span></p>
+	</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-primary" data-dismiss="modal" style="float:left">Login</button>
+</div>
+</div>
+</div>
+</divs
